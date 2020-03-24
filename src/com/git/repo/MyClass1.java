a470be08eb1d615f1cd888ef8e59f7e5c0f8264f
Mohit Sehra <mohit.sehra@intelifai.com>
2020-03-24 17:19:46 +0530

    First Commit

---
+++ src/com/git/repo/MyClass1.java
@@ -0,0 +1,14 @@
+package com.git.repo;
+
+public class MyClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s1="String";
+		String s2="string";
+		
+		System.out.println(s1.equals(s2));
+	}
+
+}
